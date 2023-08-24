c2eca0d4b0c0842df3b77945d194a4dd78daae78
lnbEli <137424458+lnbEli@users.noreply.github.com>
2023-08-24 13:19:47 +0200

    First commit, create initial function to create 16x16 grid in DOM

---
+++ script.js
@@ -0,0 +1,14 @@
+// document.body.onload = addElement;
+
+function greatSquares(num) {
+  for (let i = 0; i < num ** 2; i++) {
+    const square = document.createElement("div");
+    const grid = document.querySelector(".grid-cont");
+    const sectOne = document.querySelector(".gridLimit");
+    square.classList.add("sqStyle");
+    sectOne.style.width = `${num * 52}px`;
+    grid.appendChild(square);
+  }
+}
+
+greatSquares(16);
