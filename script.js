c4be12e42966a3cc20f72a83f29877174d98cfa9
lnbEli <137424458+lnbEli@users.noreply.github.com>
2023-08-24 23:37:55 +0200

    create width and height variables that can be accesses by js

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 // document.body.onload = addElement;
 
 function greatSquares(num) {
+  const width = 500;
+  const height = 500;
+
+  const outerSect = document.querySelector(".outerSect");
+
+  outerSect.style.width = `${width}px`;
+  outerSect.style.height = `${height}px`;
+  outerSect.style.border = "1px solid red";
+
+  //create
+
   for (let i = 0; i < num ** 2; i++) {
     const square = document.createElement("div");
     const grid = document.querySelector(".grid-cont");
     const sectOne = document.querySelector(".gridLimit");
-    square.classList.add("sqStyle");
-    sectOne.style.width = `${num * 52}px`;
+
+    square.style.minHeight = `${height / num}px`;
+    square.style.minWidth = `${width / num}px`;
+    square.style.backgroundColor = "pink";
+    square.style.border = "1px solid black";
+
+    sectOne.style.width = `${num * 25}px`; //?????
     grid.appendChild(square);
   }
 }
 
-greatSquares(100);
+greatSquares(4);
