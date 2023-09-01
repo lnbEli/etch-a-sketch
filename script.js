5f1b679b85cc9ea4882ae1427a316b3e1506b102
lnbEli <137424458+lnbEli@users.noreply.github.com>
2023-09-01 11:47:19 +0100

    Add event listener to loop that creates grid so each inndividual square changes colour with mouseover

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // document.body.onload = addElement;
 
 function greatSquares(num) {
-  const width = 650;
+  const width = 400;
   const outerSect = document.querySelector(".outerSect");
 
   outerSect.style.width = `${width}px`;
@@ -17,12 +17,19 @@ function greatSquares(num) {
 
     square.style.minHeight = `${width / num}px`;
     square.style.minWidth = `${width / num}px`;
-    square.style.backgroundColor = "pink";
+    // square.style.backgroundColor = "pink";
     square.style.border = "1px solid black";
+    square.classList.add("square");
+    square.addEventListener("mouseover", changeColor);
+
+    function changeColor() {
+      this.classList.remove("square");
+      this.classList.add("hover");
+    }
 
     sectOne.style.width = `${width}px`;
     grid.appendChild(square);
   }
 }
 
-greatSquares(9);
+greatSquares(13);
