d3ac0292a99d190cc806db7f26dd3ed502c72e8b
lnbEli <137424458+lnbEli@users.noreply.github.com>
2023-09-04 22:22:32 +0100

    Change quantity of sqaures in grid with prompt window

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
-// document.body.onload = addElement;
+//create container div for grid and add to html
+const div = document.createElement("div");
+div.classList.add("grid-cont");
+const gridLimit = document.querySelector(".gridLimit");
+gridLimit.appendChild(div);
 
+//create button and add to html
+const button = document.createElement("button");
+button.innerHTML = "Change Size";
+button.addEventListener("click", promptSize);
+const section = document.querySelector(".sectionOne");
+section.prepend(button);
+
+//create function to change size of grid
+function promptSize() {
+  let size = Number(prompt("Please grid size required (maximum 100)"));
+  while (div.firstChild) {
+    div.removeChild(div.firstChild);
+  }
+  greatSquares(size);
+}
+
+//create function to populate grid
 function greatSquares(num) {
-  const width = 700;
-  const outerSect = document.querySelector(".outerSect");
-  const section = document.querySelector(".sectionOne");
-  const button = document.createElement("button");
-  button.innerHTML = "Change Size";
-  button.addEventListener("click", promptSize);
-
-  section.prepend(button);
-  outerSect.style.width = `${width}px`;
-  outerSect.style.height = `${width}px`;
-  outerSect.style.border = "1px solid red";
-
-  //create
+  const grid = document.querySelector(".grid-cont");
+  const width = 350;
+  gridLimit.style.width = `${width}px`;
 
   for (let i = 0; i < num ** 2; i++) {
     const square = document.createElement("div");
-    const grid = document.querySelector(".grid-cont");
-    const gridLimit = document.querySelector(".gridLimit");
-
     square.style.minHeight = `${width / num}px`;
     square.style.minWidth = `${width / num}px`;
-    // square.style.backgroundColor = "pink";
     square.style.border = "1px solid black";
     square.classList.add("square");
-    square.addEventListener("mouseover", changeColor);
 
     function changeColor() {
       this.classList.remove("square");
       this.classList.add("hover");
     }
 
-    gridLimit.style.width = `${width}px`;
+    square.addEventListener("mouseover", changeColor);
+
     grid.appendChild(square);
   }
 }
 
-greatSquares(13);
+greatSquares(24);
